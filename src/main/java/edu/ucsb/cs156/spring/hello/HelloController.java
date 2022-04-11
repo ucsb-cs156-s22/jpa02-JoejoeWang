36dcb02fdc6cd8788facd189d05b2850bd7fb63e
Joe <xianqi@ucsb.edu>
2022-04-11 01:04:45 -0700

    change to email

--- src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
+++ src/main/java/edu/ucsb/cs156/spring/hello/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
     
     @RequestMapping("/")
     public String index() {
-        return "Greetings from xianqi@ucsb.edu!";
+        return "Greetings from xianqi!";
     }
     
 }
